8510c1359dadefb0112182bfe3c19a5f0ba94ed1
SNN <53225314+snndmr@users.noreply.github.com>
2019-11-15 16:39:55 +0300

    Last

--- seq.c
+++ seq.c
@@ -1,3 +1,5 @@
+#include<time.h>
+#include<math.h>
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -6,6 +8,7 @@
 void _assign(FILE *file, double *values) {
 	int index = 0;
 	for(index = 0; fscanf(file, "%lf", &*(values + index)) != EOF; index++) {}
+	fclose(file);
 }
 
 void _standardDeviation(double *values) {
@@ -18,26 +21,34 @@ void _standardDeviation(double *values) {
 		sum += *(values + index);
 	}
 	mean = sum / BUFFER;
-	printf("The mean is %.5lf\n", mean);
-
+	printf("\nThe avarage is %.5lf", mean);
+	
 	for(index = 0; index < BUFFER; index += 1) {
 		standardDeviation += pow(*(values + index) - mean, 2);
 	}
-	printf("The standard deviation is %.5lf\n", sqrt(standardDeviation / BUFFER));
+	printf("\nThe standard deviation is %.5lf\n", sqrt(standardDeviation / BUFFER));
 }
 
 int main(int argc, char *argv[]) {
 	if(argc == 2) {
+		printf("\nSequential program launched");
 		FILE *file = fopen(argv[1], "r");
 
 		if(file != NULL) {
 			double *values = (double *)malloc(BUFFER * sizeof(double));
 
+			printf("\nReading %s", argv[1]);
 			_assign(file, values);
+
+			printf("\nThe timer started.");
+            clock_t start = clock();
+            
 			_standardDeviation(values);
+			
+			clock_t difference = clock() - start;
+            printf("Calculation completed in %ld milliseconds.\n\n", (difference * 1000 / CLOCKS_PER_SEC % 1000));
 
 			free(values);
-			fclose(file);
 		} else {
 			perror("Error on source file ");
 			exit(EXIT_FAILURE);
